c3f92a9c32d5a5a0e2df5e8edb060d2378c4b7c8
Elwin <1663170148@qq.com>
2023-05-17 22:41:23 +0800

    chore: build

--- vite.config.js
+++ vite.config.js
@@ -37,19 +37,19 @@ export default defineConfig({
         drop_debugger: true
       }
     },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-        chunkFileNames: (chunkInfo) => {
-          const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
-          const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
-          return `assets/${fileName}/[name].[hash].js`;
-        }
-      }
-    }
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //       }
+    //     },
+    //     chunkFileNames: (chunkInfo) => {
+    //       const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
+    //       const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
+    //       return `js/${fileName}/[name].[hash].js`;
+    //     }
+    //   }
+    // }
   }
 })

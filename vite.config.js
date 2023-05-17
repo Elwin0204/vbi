d6dba6640a114080831d8986a89523ba0da04382
Elwin <1663170148@qq.com>
2023-05-17 22:13:12 +0800

    feat: config vite.config.js and build project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import {
   defineConfig
-} from 'vite'
-import vue from '@vitejs/plugin-vue'
+} from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,4 +17,38 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    fs: {
+      strict: false
+    }
+  },
+  build: {
+    minify: 'terser',
+    outDir: 'docs',
+    assetsDir: 'assets',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1500,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+        chunkFileNames: (chunkInfo) => {
+          const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
+          const fileName = facadeModuleId[facadeModuleId.length - 2] || '[name]';
+          return `js/${fileName}/[name].[hash].js`;
+        }
+      }
+    }
+  }
 })
